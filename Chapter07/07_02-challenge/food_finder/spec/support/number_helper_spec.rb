bde5a2006f16b9f64453249c640a35df2ea85689
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-06-08 09:40:44 -0700

    Challenge: Number Helper save point

--- Chapter07/07_02-challenge/food_finder/spec/support/number_helper_spec.rb
+++ Chapter07/07_02-challenge/food_finder/spec/support/number_helper_spec.rb
@@ -1,5 +1,8 @@
 # encoding: UTF-8
 
+#require 'lib/support/number_helper' DONT NEED TO
+#run with (from 07_02-challenge/food_finder) rspec spec/support/number_helper_spec.rb
+
 describe 'NumberHelper' do
 
   include NumberHelper
@@ -8,15 +11,25 @@ describe 'NumberHelper' do
     
     context 'using default values' do
       
-      it "correctly formats an integer"
+      it "correctly formats an integer" do
+        expect(number_to_currency(2000)).to eq('$2,000.00')
+      end
       
-      it "correctly formats a float"
+      it "correctly formats a float" do
+        expect(number_to_currency(2000.00)).to eq('$2,000.00')
+      end
 
-      it "correctly formats a string"
+      it "correctly formats a string" do
+        expect(number_to_currency('2000')).to eq('$2,000.00')
+      end
       
-      it "uses delimiters for very large numbers"
+      it "uses delimiters for very large numbers" do
+        expect(number_to_currency(200000)).to eq('$200,000.00')
+      end
 
-      it "does not have delimiters for small numbers"
+      it "does not have delimiters for small numbers" do
+        expect(number_to_currency(2)).to eq('$2.00')
+      end
 
     end
     
