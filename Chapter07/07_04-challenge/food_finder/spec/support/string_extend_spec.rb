4373129afad407ab8b428e6bdee1ea1a12dd3597
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-06-08 21:41:33 -0700

    StringExtend

--- Chapter07/07_04-challenge/food_finder/spec/support/string_extend_spec.rb
+++ Chapter07/07_04-challenge/food_finder/spec/support/string_extend_spec.rb
@@ -2,31 +2,51 @@ describe 'String' do
 
   describe "#titleize" do
 
-    it "capitalizes each word in a string"
+    it "capitalizes each word in a string" do 
+      expect('one two three'.titleize).to eq('One Two Three')
+    end
     
-    it "works with single-word strings"
+    it "works with single-word strings" do
+      expect('one'.titleize).to eq('One')
+    end
     
-    it "capitalizes all uppercase strings"
+    it "capitalizes all uppercase strings" do 
+      expect('ONE'.titleize).to eq('One')
+    end
     
-    it "capitalizes mixed-case strings"
+    it "capitalizes mixed-case strings" do
+      expect('oNe'.titleize).to eq('One')
+    end
     
   end
   
   describe '#blank?' do
 
-    it "returns true if string is empty"
+    it "returns true if string is empty" do
+      expect(''.blank?).to eq(true)
+    end
 
-    it "returns true if string contains only spaces"
+    it "returns true if string contains only spaces" do 
+      expect('  '.blank?).to eq(true)
+    end
 
-    it "returns true if string contains only tabs"
+    it "returns true if string contains only tabs" do
     # Get a tab using a double-quoted string with \t
     # example: "\t\t\t"
+      expect("\t\t\t".blank?).to eq(true)  
+    end
 
-    it "returns true if string contains only spaces and tabs"
+    it "returns true if string contains only spaces and tabs" do
+      expect("\t\t\t ".blank?).to eq(true)
+    end
     
-    it "returns false if string contains a letter"
+    it "returns false if string contains a letter" do
+      expect('a'.blank?).to eq(false)
+    end
 
-    it "returns false if string contains a number"
+    it "returns false if string contains a number" do
+      expect('1'.blank?).to eq(false)
+    end
     
   end
   
