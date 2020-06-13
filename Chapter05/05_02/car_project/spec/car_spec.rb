fbc3b48302fcc43ef99dd5cf00644d62c533ed29
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-06-13 07:52:18 -0700

    Hooks: before, after, and around

--- Chapter05/05_02/car_project/spec/car_spec.rb
+++ Chapter05/05_02/car_project/spec/car_spec.rb
@@ -2,32 +2,38 @@ require 'car'
 
 describe 'Car' do
 
+  before(:example) do
+    @car = Car.new
+  end 
+
   describe 'attributes' do
     
     it "allows reading and writing for :make" do
-      car = Car.new
-      car.make = 'Test'
-      expect(car.make).to eq('Test')
+      #car = Car.new
+      @car.make = 'Test'
+      expect(@car.make).to eq('Test')
     end
     
     it "allows reading and writing for :year" do
-      car = Car.new
-      car.year = 9999
-      expect(car.year).to eq(9999)
+      #car = Car.new
+      @car.year = 9999
+      expect(@car.year).to eq(9999)
     end
     
     it "allows reading and writing for :color" do
-      car = Car.new
-      car.color = 'foo'
-      expect(car.color).to eq('foo')
+      #car = Car.new
+      @car.color = 'foo'
+      expect(@car.color).to eq('foo')
     end
     
     it "allows reading for :wheels" do
-      car = Car.new
-      expect(car.wheels).to eq(4)
+      #car = Car.new
+      expect(@car.wheels).to eq(4)
     end
     
-    it "allows writing for :doors"
+    it "allows writing for :doors" do
+      expect(@car.doors).to eq()
+    end
     
   end
   
