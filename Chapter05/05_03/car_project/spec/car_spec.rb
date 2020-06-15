0159784f7b284ae3d3157a91965149cc114b6058
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-06-15 06:48:54 -0700

    Using the let method

--- Chapter05/05_03/car_project/spec/car_spec.rb
+++ Chapter05/05_03/car_project/spec/car_spec.rb
@@ -4,27 +4,31 @@ describe 'Car' do
 
   describe 'attributes' do
     
-    before(:example) do
-      @car = Car.new
-    end
+    let(:car) { Car.new }
+    # before(:example) do
+    #   @car = Car.new
+    # end
     
     it "allows reading and writing for :make" do
-      @car.make = 'Test'
-      expect(@car.make).to eq('Test')
+      # @car.make = 'Test'
+      car.make = 'Test'
+      expect(car.make).to eq('Test')
     end
     
     it "allows reading and writing for :year" do
-      @car.year = 9999
-      expect(@car.year).to eq(9999)
+     #@car.year = 9999
+     car.year = 9999
+      expect(car.year).to eq(9999)
     end
     
     it "allows reading and writing for :color" do
-      @car.color = 'foo'
-      expect(@car.color).to eq('foo')
+      #@car.color = 'foo'
+      car.color = 'foo'
+      expect(car.color).to eq('foo')
     end
     
     it "allows reading for :wheels" do
-      expect(@car.wheels).to eq(4)
+      expect(car.wheels).to eq(4)
     end
     
     it "allows writing for :doors"
@@ -33,24 +37,29 @@ describe 'Car' do
   
   describe '.colors' do
 
+    let(:colors) {['blue', 'black', 'red', 'green']}
     it "returns an array of color names" do
-      c = ['blue', 'black', 'red', 'green']
-      expect(Car.colors).to match_array(c)
+      #c = ['blue', 'black', 'red', 'green']
+      #expect(Car.colors).to match_array(c)
+      expect(Car.colors).to match_array(colors)
     end
 
   end
   
   describe '#full_name' do
-    
+    let(:new_car) { Car.new }
+    let(:honda) { Car.new(:make => 'Honda', :year => 2004, :color => 'blue')}
     it "returns a string in the expected format" do
-      @honda = Car.new(:make => 'Honda', :year => 2004, :color => 'blue')
-      expect(@honda.full_name).to eq('2004 Honda (blue)')
+      #@honda = Car.new(:make => 'Honda', :year => 2004, :color => 'blue')
+      #expect(@honda.full_name).to eq('2004 Honda (blue)')
+      expect(honda.full_name).to eq('2004 Honda (blue)')
     end
     
     context 'when initialized with no arguments' do
       it 'returns a string using default values' do
-        car = Car.new
-        expect(car.full_name).to eq('2007 Volvo (unknown)')
+        #car = Car.new
+        #expect(car.full_name).to eq('2007 Volvo (unknown)')
+        expect(new_car.full_name).to eq('2007 Volvo (unknown)')
       end
     end
 
